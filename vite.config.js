ea2dec880c7749268428274b170c466262c977e2
Vinaya <vinayarajput0716@gmail.com>
2025-01-25 18:42:04 +0530

    1st change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/Card-game-react/', // Base path for deployment
+  plugins: [react()],       // React plugin for Vite
+});
